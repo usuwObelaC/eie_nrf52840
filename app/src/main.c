dac7094a08746c189f16702eb9e1c94f1cfbee73
CALEB_OWUSU <usuwObleaC>
2026-01-15 20:13:36 -0700

    lesson 9

--- app/src/main.c
+++ app/src/main.c
@@ -9,20 +9,111 @@
 
 #include "BTN.h"
 #include "LED.h"
+#include <errno.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/types.h>
 
 #define SLEEP_MS 1
 
-int main(void) {
+static struct bt_uuid_128 BLE_CUSTOM_SERVICE_UUID =
+    BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000000001));
+static struct bt_uuid_128 BLE_CUSTOM_CHARACTERISTIC_UUID =
+    BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000000002));
+static struct bt_conn* my_connection;    
 
-  if (0 > BTN_init()) {
-    return 0;
+static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
+   if (err != 0) {
+    bt_conn_unref(my_connection); //no idea
+    my_connection = NULL;
   }
-  if (0 > LED_init()) {
-    return 0;
+  if (conn != my_connection){
+    bt_conn_unref(my_connection); //no idea
+    my_connection = NULL;
   }
-
-  while(1) {
-    k_msleep(SLEEP_MS);
+  else{
+    char addr[BT_ADDR_LE_STR_LEN];
+    bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+    printk("Connected: %s\n", addr);
   }
-	return 0;
 }
+
+static void ble_on_device_disconnected(struct bt_conn* conn, uint8_t reason) {
+ if (conn != my_connection){
+    bt_conn_unref(my_connection); //no idea
+    my_connection = NULL;
+  }
+  else{
+    printk(bt_hci_err_to_str(reason));
+  }
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+    .connected = ble_on_device_connected,
+    .disconnected = ble_on_device_disconnected,
+};
+
+static bool ble_get_adv_device_name_cb(struct bt_data* data, void* user_data) {
+  char* name = user_data;
+
+  if (data->type == BT_DATA_NAME_COMPLETE || data->type == BT_DATA_NAME_SHORTENED) {
+    memcpy(name, data->data, data->data_len);
+    name[data->data_len] = 0; // Null-terminator
+    return false; // Stop parsing
+  }
+
+  return true; // Continue looking through this advertising packet
+}
+static void ble_on_advertisement_received(const bt_addr_le_t* addr, int8_t rssi, uint8_t type,
+                                          struct net_buf_simple* ad) {
+  if(my_connection != NULL){
+    return;
+  }
+  else if(type != BT_GAP_ADV_TYPE_ADV_IND ||type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND){
+    return;
+  }
+  else{
+    char name[32] = {0};
+    bt_data_parse(ad, ble_get_adv_device_name_cb, name);
+    printk("%s",name);
+    printk("%p", addr);
+    bt_addr_le_to_str(addr, ad, sizeof(ad));// not sure if this is right
+    printk("Connected: %s\n", ad);
+    if(rssi < -50){
+      return;
+    }
+    else{
+      bt_le_scan_stop();
+      bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT, &my_connection);
+      if(bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT, &my_connection) != 0){
+        bt_conn_unref(my_connection); //no idea
+        my_connection = NULL;
+      }
+
+    }
+    
+  }
+}
+
+  
+  int main(void) {
+  
+  int err = bt_enable(NULL);
+  if (err) {
+    printk("Bluetooth init failed (err %d)\n", err);
+    return 0;
+  } else {
+    printk("Bluetooth initialized\n");
+  }
+  bt_le_scan_start(BT_LE_SCAN_PASSIVE, ble_on_advertisement_received);
+}
+
