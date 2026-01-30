26661451c95d455132eb15e2a17368e07208c141
CALEB_OWUSU <usuwObleaC>
2026-01-29 19:58:04 -0700

    lesson 11 part 1

--- app/src/main.c
+++ app/src/main.c
@@ -3,61 +3,30 @@
  */
 
 #include <inttypes.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/spi.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/display.h>
 #include <zephyr/sys/printk.h>
+
+#include <lvgl.h>
 
 #include "BTN.h"
 #include "LED.h"
+#include "lv_data_obj.h"
 
-#define CMD_SOFTWARE_RESET 0x01
-#define CMD_SLEEP_OUT 0x11
-#define CMD_DISPLAY_ON 0x29
-#define CMD_COLUMN_ADDRESS_SET 0x2A
-#define CMD_ROW_ADDRESS_SET 0x2B
-#define CMD_MEMORY_WRITE 0x2C
 
 #define SLEEP_MS 1
 
-#define ARDUINO_SPI_NODE DT_NODELABEL(arduino_spi)
-#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_isplay));
+static lvl_obj_t *screen =NULL;
 
-static const struct gpio_dt_spec dcx_gpio = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, dcx_gpios);
-static const struct spi_cs_control cs_ctrl = (struct spi_cs_control){
-  .gpio = GPIO_DT_SPEC_GET(ARDUINO_SPI_NODE, cs_gpios),
-  .delay = 1u,
-};
-static const struct device * dev = DEVICE_DT_GET(ARDUINO_SPI_NODE);
-static const struct spi_config spi_cfg ={
-  .frequency =1000000,
-  .operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
-  .slave = 0,
-  .cs = cs_ctrl
-};
-
-static void lcd_cmd(uint8_t cmd, struct spi_buf * data){
-  struct spi_buf cmd_buf[1] ={{&cmd, 1}};
-  struct spi_buf_set cmd_set = {cmd_buf, 1};
-  
-  gpio_pin_set_dt(&dcx_gpio, 0);
-  spi_write(dev, &spi_cfg, &cmd_set);
-
-  if (data != NULL){
-    struct spi_buf_set data_set = {data, 1};
-    gpio_pin_set_dt(&dcx_gpio, 1);
-    spi_write(dev, &spi_cfg, &data_set);
-  }
-}
 int main(void) {
-   if (!device_is_ready(dev)) {
+   if (!device_is_ready(display_dev)) {
     return 0;
   }
-   if (!gpio_is_ready_dt(&dcx_gpio)) {
-    return 0;
-  }
-   if (gpio_pin_configure_dt(&dcx_gpio, GPIO_OUTPUT_LOW)) {
+  screen = lv_screen_active();
+  if (screen = NULL) {
     return 0;
   }
   if (0 > BTN_init()) {
@@ -66,30 +35,13 @@ int main(void) {
   if (0 > LED_init()) {
     return 0;
   }
+  lv_obj_t *label = lv_label_create(screen);
+  lv_label_set_text(label, "HEllo World");
+  display_blanking_off(display_dev);
 
-  lcd_cmd(CMD_SOFTWARE_RESET, NULL);
-  k_msleep(120);
-  lcd_cmd(CMD_SLEEP_OUT, NULL);
-  lcd_cmd(CMD_DISPLAY_ON, NULL);
-
-
-  uint8_t column_data[] = {0x00,0x95,0x00,0x9f};
-  uint8_t row_data[] = {0x00,0x75,0x00,0x7f};
-  uint8_t color_data[300];
-  for(int i = 0; i< 300; i+=3){
-    color_data[i] = 0xFC;
-    color_data[i+1] = 0;
-    color_data[i+2] = 0;
-  }
-  struct spi_buf column_data_buf = {column_data, 4};
-  struct spi_buf row_data_buf = {row_data, 4};
-  struct spi_buf color_data_buf = {color_data, 300};
-
-  lcd_cmd(CMD_COLUMN_ADDRESS_SET, &column_data_buf);
-  lcd_cmd(CMD_ROW_ADDRESS_SET, &row_data_buf);
-  lcd_cmd(CMD_MEMORY_WRITE, &color_data_buf);
   
   while(1) {
+    lv_timer_handler();
     k_msleep(SLEEP_MS);
   }
   return 0;
