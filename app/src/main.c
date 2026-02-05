d920158a7efb8b86bf2ce10b75f0bc130fe8226f
CALEB_OWUSU <usuwObleaC>
2026-02-04 20:50:07 -0700

    LED buttons

--- app/src/main.c
+++ app/src/main.c
@@ -20,6 +20,13 @@
 static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 static lv_obj_t *screen = NULL;
 
+void lv_button_callback(lv_event_t *event){
+  lv_obj_t *data_obj = (lv_obj_t *)lv_event_get_user_data(event);
+  led_id led = *(led_id *)lv_data_obj_get_data_ptr(data_obj);
+
+  LED_toggle(led);
+}
+
 int main(void) {
    if (!device_is_ready(display_dev)) {
     return 0;
@@ -43,6 +50,10 @@ int main(void) {
     snprintf(label_text, 10, "LED %d", i);
     lv_label_set_text(button_label, label_text);
     lv_obj_align(button_label,LV_ALIGN_CENTER, 0,0);
+
+    led_id led = (led_id)i;
+    lv_obj_t* data_obj = lv_data_obj_create_alloc_assign(ui_btn, &led, sizeof(led_id));
+    lv_obj_add_event_cb(ui_btn, lv_button_callback, LV_EVENT_CLICKED, data_obj);
   }
   
   display_blanking_off(display_dev);
