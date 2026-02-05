38d702d8f97a2918234c85e85874203720975467
CALEB_OWUSU <usuwObleaC>
2026-02-04 21:42:47 -0700

    puppy image

--- app/src/main.c
+++ app/src/main.c
@@ -20,12 +20,7 @@
 static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 static lv_obj_t *screen = NULL;
 
-void lv_button_callback(lv_event_t *event){
-  lv_obj_t *data_obj = (lv_obj_t *)lv_event_get_user_data(event);
-  led_id led = *(led_id *)lv_data_obj_get_data_ptr(data_obj);
-
-  LED_toggle(led);
-}
+extern const lv_image_dsc_t cute_puppy;
 
 int main(void) {
    if (!device_is_ready(display_dev)) {
@@ -41,20 +36,9 @@ int main(void) {
   if (0 > LED_init()) {
     return 0;
   }
-  for(uint8_t i = 0; i < NUM_LEDS; i++){
-    lv_obj_t *ui_btn = lv_button_create(screen);
-
-    lv_obj_align(ui_btn, LV_ALIGN_CENTER, 50 * (i % 2 ? 1 : -1), 20 * (i < 2 ? -1 : 1));
-    lv_obj_t *button_label = lv_label_create(ui_btn);
-    char label_text[10];
-    snprintf(label_text, 10, "LED %d", i);
-    lv_label_set_text(button_label, label_text);
-    lv_obj_align(button_label,LV_ALIGN_CENTER, 0,0);
-
-    led_id led = (led_id)i;
-    lv_obj_t* data_obj = lv_data_obj_create_alloc_assign(ui_btn, &led, sizeof(led_id));
-    lv_obj_add_event_cb(ui_btn, lv_button_callback, LV_EVENT_CLICKED, data_obj);
-  }
+  lv_obj_t *image = lv_image_create(screen);
+  lv_image_set_src(image, &cute_puppy);
+  lv_obj_align(image, LV_ALIGN_CENTER, 0, 0);
   
   display_blanking_off(display_dev);
 
